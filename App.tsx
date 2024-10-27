fc689ebd8198b933410d2325f7f2ddcbcde1d931
stepan.rutz <stepan.rutz@gmx.de>
2024-10-27 13:51:37 +0100

    empty project, without router

--- App.tsx
+++ App.tsx
@@ -1,20 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert, Button, SafeAreaView, Text, View } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+
+    const handleButton = () => {
+        Alert.alert(
+            'Question',
+            'Are you sure you want to proceed?',
+            [
+                {
+                    text: 'Yes',
+                    onPress: () => console.log('Yes pressed'),
+                },
+            ],
+            { cancelable: false } // kein Tap auf den Hintergrund zum Schließen
+        )
+    }
+
+    return (
+        <SafeAreaView className="flex-1 bg-gray-200 items-center justify-center">
+            <View className="flex-1 w-full bg-gray-200 items-center justify-center">
+                <Text className="text-4xl">Hello World</Text>
+                <View className="py-4">
+                    <Button onPress={handleButton} title="Click me"></Button>
+                </View>
+                <StatusBar style="auto" />
+            </View>
+        </SafeAreaView>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
