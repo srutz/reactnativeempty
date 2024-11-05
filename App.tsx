2b5466bede0d0d9cb2a8d73067621a486f3383e5
stepan.rutz <stepan.rutz@gmx.de>
2024-11-05 11:40:58 +0100

    preparing image loop

--- App.tsx
+++ App.tsx
@@ -2,15 +2,20 @@ import { StatusBar } from 'expo-status-bar';
 import { ReactNode } from 'react';
 import { Alert, Button, Image, SafeAreaView, ScrollView, Text, View } from 'react-native';
 
-const product = {
-    image: "https://cdn.dummyjson.com/products/images/furniture/Knoll%20Saarinen%20Executive%20Conference%20Chair/thumbnail.png",
+const mainProduct = {
+    image: [
+        "https://cdn.dummyjson.com/products/images/furniture/Knoll%20Saarinen%20Executive%20Conference%20Chair/thumbnail.png",
+        "https://cdn.dummyjson.com/products/images/furniture/Annibale%20Colombo%20Sofa/thumbnail.png",
+        "https://cdn.dummyjson.com/products/images/groceries/Cat%20Food/thumbnail.png",
+        "https://cdn.dummyjson.com/products/images/groceries/Green%20Bell%20Pepper/thumbnail.png",
+    ],
     price: 99910.50,
     title: "Sessel",
-    description: "Dieser Sessel ist ein super komfortabler Sessel, der Ihnen ein tolles Sitzerlebnis bescheren wird.",
+    description: "Dieser Sessel ist ein super komfortabler Sessel, der Ihnen ein tolles Sitzerlebnis bescheren wird." + "Dieser Sessel ist ein super komfortabler Sessel, der Ihnen ein tolles Sitzerlebnis bescheren wird.",
     stockAmount: 6,
     rating: 3,
 }
-type ProductType = typeof product
+type ProductType = typeof mainProduct
 
 /* format number as german money, using intl */
 function formatGermanMoney(price: number) {
@@ -26,28 +31,43 @@ export default function App() {
     */
     return (
         <SafeAreaView className="flex-1 bg-gray-200 items-center justify-center">
-            <View className="bg-white p-4 h-[300] w-[85%] rounded-lg">
-                <View className="flex flex-row justify-between">
+            <ProductView product={mainProduct} />
+        </SafeAreaView>
+    )
+}
+
+type ProductViewPropsType = {
+    product: ProductType
+}
+
+function ProductView(props: ProductViewPropsType) {
+    const { product } = props
+    return (
+        <View className="bg-white p-4 mb-4 mx-4 w-[80%] h-[350] rounded-lg">
+            <View className="flex flex-row justify-between">
+                <View className="bg-gray-200 rounded-lg m-2">
                     <Image className="h-32 w-32" resizeMode="contain" source={{ uri: product.image }} ></Image>
-                    <View className="self-end pb-4">
-                        <Text className="text-2xl">{formatGermanMoney(product.price)}</Text>
-                    </View>
-                </View>
-                <View className="mb-2"><Text className="font-bold">{product.title}</Text></View>
-                <View className="grow mb-2"><Text>{product.description}</Text></View>
-                <View className="flex flex-row border-t border-gray-300 pt-2">
-                    <View className="">
-                        <Text>Lieferbare Anzahl:
-                            <Text className="font-bold"> {product.stockAmount}</Text>
-                        </Text>
-                    </View>
-                    <View className="grow"></View>
-                    <View className=""><Text>Bewertung: 
-                        <Text className="font-bold">{product.rating} von 5</Text>
-                    </Text></View>
                 </View>
             </View>
-        </SafeAreaView>
+            <View className="mb-2"><Text className="font-bold">{product.title}</Text></View>
+            <View className="flex-1 mb-2">
+                <Text numberOfLines={4}>{product.description}</Text>
+            </View>
+            <View className="self-end pb-4">
+                <Text className="text-xl">{formatGermanMoney(product.price)}</Text>
+            </View>
+            <View className="flex flex-row border-t border-gray-300 pt-2">
+                <View className="">
+                    <Text>Lieferbare Anzahl:
+                        <Text className="font-bold"> {product.stockAmount}</Text>
+                    </Text>
+                </View>
+                <View className="grow"></View>
+                <View className=""><Text>Bewertung: 
+                    <Text className="font-bold">{product.rating} von 5</Text>
+                </Text></View>
+            </View>
+        </View>
     )
 }
 
