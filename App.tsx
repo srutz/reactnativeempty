bcd93483bbde85d0dc2ee0233a439f7c8010dbc1
stepan.rutz <stepan.rutz@gmx.de>
2024-11-06 11:15:09 +0100

    Revert "added useCounter custom hook and implement a clock"
    This reverts commit 7c222b88d311fedc344cf3358b3521be39c3c0a4.

--- App.tsx
+++ App.tsx
@@ -1,19 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { Alert, Button, SafeAreaView, Text, View } from 'react-native';
 import { SafeContainer } from './components/SafeContainer';
-import { useEffect, useState } from 'react';
-import { useCounter } from './hooks/useCounter';
-
 
 export default function App() {
-    useCounter(750)
+
     const handleButton = () => {
+        Alert.alert(
+            'Question',
+            'Are you sure you want to proceed?',
+            [
+                {
+                    text: 'Yes',
+                    onPress: () => console.log('Yes pressed'),
+                },
+            ],
+            { cancelable: false } // kein Tap auf den Hintergrund zum Schließen
+        )
     }
-    console.log("---rendering App " + new Date().toLocaleString())
+
     return (
         <SafeContainer>
             <View className="flex-1 w-full bg-gray-200 items-center justify-start">
-                <Text className="text-4xl">Hello World {new Date().toLocaleString()}</Text>
+                <Text className="text-4xl">Hello World</Text>
                 <View className="py-4">
                     <Button onPress={handleButton} title="Click me"></Button>
                 </View>
