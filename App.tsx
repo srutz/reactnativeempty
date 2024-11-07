d72c47dd409f80485b5246cd40f44224059a8f62
stepan.rutz <stepan.rutz@gmx.de>
2024-11-07 12:00:33 +0100

    first screen, package json with version6 of navigation

--- App.tsx
+++ App.tsx
@@ -1,33 +1,16 @@
-import { StatusBar } from 'expo-status-bar';
-import { Alert, Button, SafeAreaView, Text, View } from 'react-native';
-import { SafeContainer } from './components/SafeContainer';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { HomeScreen } from './components/screens/HomeScreen';
+
+const Stack = createNativeStackNavigator()
 
 export default function App() {
-
-    const handleButton = () => {
-        Alert.alert(
-            'Question',
-            'Are you sure you want to proceed?',
-            [
-                {
-                    text: 'Yes',
-                    onPress: () => console.log('Yes pressed'),
-                },
-            ],
-            { cancelable: false } // kein Tap auf den Hintergrund zum Schließen
-        )
-    }
-
     return (
-        <SafeContainer>
-            <View className="flex-1 w-full bg-gray-200 items-center justify-start">
-                <Text className="text-4xl">Hello World</Text>
-                <View className="py-4">
-                    <Button onPress={handleButton} title="Click me"></Button>
-                </View>
-                <StatusBar style="auto" />
-            </View>
-        </SafeContainer>
+        <NavigationContainer>
+            <Stack.Navigator>
+                <Stack.Screen name="Home" component={HomeScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
     )
 }
 
---
+++ components/screens/HomeScreen.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export function HomeScreen() {
+    return (
+        <View className="flex-1 flex flex-col bg-green-400 justify-center items-center">
+            <Text>Home Screen</Text>
+        </View>
+    )
+}
