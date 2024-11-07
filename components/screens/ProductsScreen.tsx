64240ef60c5d97209c992ea780597712aea6e481
stepan.rutz <stepan.rutz@gmx.de>
2024-11-07 13:54:51 +0100

    productsscreen

--- components/screens/ProductsScreen.tsx
+++ components/screens/ProductsScreen.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { FlatList, Image, Text, View } from "react-native";
+import { FlatList, Image, Pressable, Text, View } from "react-native";
 import { Product, ProductsResponse } from "../Types";
 
 export function ProductsScreen() {
@@ -15,13 +15,14 @@ export function ProductsScreen() {
     return (
         <FlatList data={products}
             keyExtractor={(product) => product.id.toString()}
-            renderItem={(info) => (<ProductsItem product={info.item}></ProductsItem>) }
+            renderItem={(info) => (<ProductsItem product={info.item} />) }
         ></FlatList>
     )
 }
 
 /* Rendert das übergebene Product */
 function ProductsItem({ product } : { product: Product}) {
+    const handleButton = () => { console.log("hi " + product.title)}
     return (
         <View className="flex flex-row p-2 m-3 bg-white rounded-xl border border-gray-300 shadow shadow-black">
             <View className="mr-4 flex justify-center border-r border-gray-300">
@@ -30,8 +31,21 @@ function ProductsItem({ product } : { product: Product}) {
             <View className="flex-1 flex flex-col pb-2">
                 <Text className="text-lg">{product.title}</Text>
                 <Text className="text-gray-700">{product.description}</Text>
+                <View className="self-end mt-2">
+                    <MyButton title="Show more" onPress={handleButton}></MyButton>
+                </View>
             </View>
         </View>
+    )
+}
+
+function MyButton({ title, onPress }: { title: string, onPress: () => void}) {
+    return (
+        <Pressable onPress={onPress}>
+            <View className="bg-blue-700 px-2 py-1 rounded m-1">
+                <Text className="uppercase text-sm text-white">{title}</Text>
+            </View>
+        </Pressable>
     )
 }
 
