6727296fc6d6b8c7dfe0ef25b71fb1df61032d84
stepan.rutz <stepan.rutz@gmx.de>
2024-11-08 14:16:27 +0100

    Status bar

--- App.tsx
+++ App.tsx
@@ -1,8 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import * as ScreenOrientation from 'expo-screen-orientation';
+import { StatusBar } from 'expo-status-bar';
 import { useEffect } from 'react';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { SafeContainer } from './components/SafeContainer';
 import { HomeScreen } from './components/screens/HomeScreen';
 import { ProductDetails } from './components/screens/ProductDetails';
 import { ProductsScreen } from './components/screens/ProductsScreen';
@@ -22,15 +24,18 @@ export default function App() {
         ScreenOrientation.unlockAsync()
     }, [])
     return (
-        <GestureHandlerRootView>
-            <NavigationContainer>
-                <Stack.Navigator >
-                    <Stack.Screen name="Home" component={HomeScreen} options={ { headerShown: false }} />
-                    <Stack.Screen name="Products" component={ProductsScreen} options={ { headerShown: false }} />
-                    <Stack.Screen name="ProductDetails" component={ProductDetails} options={ { headerShown: false }} />
-                </Stack.Navigator>
-            </NavigationContainer>
-        </GestureHandlerRootView>
+        <SafeContainer>
+            <StatusBar></StatusBar>
+            <GestureHandlerRootView>
+                <NavigationContainer>
+                    <Stack.Navigator >
+                        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
+                        <Stack.Screen name="Products" component={ProductsScreen} options={{ headerShown: false }} />
+                        <Stack.Screen name="ProductDetails" component={ProductDetails} options={{ headerShown: false }} />
+                    </Stack.Navigator>
+                </NavigationContainer>
+            </GestureHandlerRootView>
+        </SafeContainer>
     )
 }
 
--- components/screens/HomeScreen.tsx
+++ components/screens/HomeScreen.tsx
@@ -1,6 +1,6 @@
 import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { Orientation } from "expo-screen-orientation";
-import { Button, View } from "react-native";
+import { Button, Text, View } from "react-native";
 import * as Animatable from "react-native-animatable";
 import { ScreenTypes } from "../../App";
 import { useDimension } from "../../hooks/useDimension";
@@ -29,15 +29,21 @@ export function HomeScreen() {
             <View className="mt-4 mb-2">
                 <Button title="Go to products" onPress={handleButton}></Button>
             </View>
-            <View className="w-full h-1 grow flex flex-col overflow-hidden">
+            <View className="w-full h-1 grow flex flex-col overflow-hidden relative">
                 {orientation == Orientation.LANDSCAPE_LEFT || orientation == Orientation.LANDSCAPE_RIGHT
-                ? <Animatable.Image 
-                    animation="bounceInDown"
-                    className="w-full flex-1" resizeMode="cover" source={berlinBreit} />
-                : <Animatable.Image 
-                    animation="bounceInUp"
-                    className="w-full flex-1" resizeMode="cover" source={berlinHoch} />
+                    ? <Animatable.Image
+                        animation="bounceInDown"
+                        className="w-full flex-1" resizeMode="cover" source={berlinBreit} />
+                    : <Animatable.Image
+                        animation="bounceInUp"
+                        className="w-full flex-1" resizeMode="cover" source={berlinHoch} />
                 }
+                <View className="bg-black opacity-50 absolute p-8 top-[40%] w-full">
+                    <Text className="uppercase text-4xl text-white text-center">Hello Berlin</Text>
+                </View>
+                <View className="absolute p-8 top-[40%] w-full">
+                    <Text className="uppercase text-4xl text-white text-center">Hello Berlin</Text>
+                </View>
             </View>
             <MyBottomSheet height={32}>
                 <View className="mt-4 mb-2 self-center">
