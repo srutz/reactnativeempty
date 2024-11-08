c594bec50b69d864d7d3911479236fc0b54017c7
stepan.rutz <stepan.rutz@gmx.de>
2024-11-08 10:38:14 +0100

    added useScreenOrientation custom hook

--- components/screens/HomeScreen.tsx
+++ components/screens/HomeScreen.tsx
@@ -1,17 +1,20 @@
-import { NavigationProp, useNavigation, useRoute } from "@react-navigation/native";
+import { NavigationProp, useNavigation } from "@react-navigation/native";
 import { Button, Text, View } from "react-native";
 import { ScreenTypes } from "../../App";
 import { useDimension } from "../../hooks/useDimension";
+import { orientationToString, useScreenOrientation } from "../../hooks/useScreenOrientation";
 
 export function HomeScreen() {
     const nav = useNavigation<NavigationProp<ScreenTypes>>();
     const handleButton = () => {
         nav.navigate("Products")
     }
-    const route = useRoute()
     const dimension = useDimension()  // wir bekommen die aktuelle Größe und sind reaktiv
+    const orientation = useScreenOrientation()
+    console.log(orientationToString(orientation))
     return (
         <View className="flex-1 flex flex-col bg-green-400 justify-center items-center">
+            <Text>{orientationToString(orientation)}</Text>
             <Text>Dim {JSON.stringify(dimension, null, 4)}</Text>
             <Button title="Go to products" onPress={handleButton}></Button>
         </View>
--- hooks/useScreenOrientation.tsx
+++ hooks/useScreenOrientation.tsx
@@ -1,11 +1,39 @@
-import { useEffect } from "react"
+import * as ScreenOrientation from "expo-screen-orientation"
+import { useEffect, useState } from "react"
 
 export function useScreenOrientation() {
 
+    const[orientation,setOrientation] = useState<ScreenOrientation.Orientation>()
+
     useEffect(() => {
         /* registration */
+        const listener = async () => {
+            const o = await ScreenOrientation.getOrientationAsync()
+            setOrientation(o)
+        }
+        listener()  // wert einmalig setzen
+        const subscription = ScreenOrientation.addOrientationChangeListener(listener)
         return () => {
             /* cleanup */
+            subscription.remove()
         }
     }, []) // wichtig, Dependency Array nicht vergessen
+    return orientation
+}
+
+export function orientationToString(orientation?: ScreenOrientation.Orientation) {
+    switch(orientation) {
+        case ScreenOrientation.Orientation.UNKNOWN:
+            return "Unknown"
+        case ScreenOrientation.Orientation.LANDSCAPE_LEFT:
+            return "Landscape Left"
+        case ScreenOrientation.Orientation.LANDSCAPE_RIGHT:
+            return "Landscape Right"
+        case ScreenOrientation.Orientation.PORTRAIT_DOWN:
+            return "Portrait Down"
+        case ScreenOrientation.Orientation.PORTRAIT_UP:
+            return "Portrait Up"
+        default:
+            return "Not initialized"
+    }
 }
